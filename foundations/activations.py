fb0be277fbffe1ba307dcc628ad43b5906c16af4
Gabriel <122925386+gabifrez@users.noreply.github.com>
2026-06-27 01:05:35 +0300

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -8,14 +8,9 @@ class Solution:
         # z is a 1D NumPy array
         # Formula: 1 / (1 + e^(-z))
         # return np.round(your_answer, 5)
-        for i in range(len(z)):
-            z[i] = 1 / (1 + np.exp(-z[i]))
-            z[i] = np.round(z[i], 5)
-        return z
+        return np.round(1/(1+np.exp(-z)), 5)
 
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
         # Formula: max(0, z) element-wise
-        for i in range(len(z)):
-            z[i] = max(0, z[i])
-        return z
+        return np.maximum(0,z)
